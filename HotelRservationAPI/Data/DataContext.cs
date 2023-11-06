d0bad093d679c963cb71f4c376626d08f4670ea1
IHateParbal <123918206+IHateParbal@users.noreply.github.com>
2023-11-06 13:07:35 +0545

    Migration Added

--- HotelRservationAPI/Data/DataContext.cs
+++ HotelRservationAPI/Data/DataContext.cs
@@ -13,5 +13,13 @@ namespace HotelRservationAPI.Data
         public DbSet<Reservation>  reservations { get; set; }
         public DbSet<Room> rooms { get; set; }
         public DbSet<Register> registers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Register>()
+                .HasNoKey();
+        }
     }
 }
